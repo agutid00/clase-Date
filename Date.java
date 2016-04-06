dc5f231498d430b4190e0a123c3773c082783498
agutid00 <agutid00@estudiantes.unileon.es>
2016-04-06 11:21:34 +0200

    6 commit

--- Date.java
+++ Date.java
@@ -1,92 +1,103 @@
-class Date{
-	int year;
-	int month;
-	int day;
+public class Date{
+	private int year;
+	private int month;
+	private int day;
 
-	Public Date (int day,int month,int year){
+	public Date (int day,int month,int year){
 		this.day=day;
 		this.month=month;
 		this.year=year;
 		}
-		boolean isSameYear (Date otherDate){
-			return this.year==otherDate.getYear();
+	boolean isSameYear (Date otherDate){
+			return this.year==otherDate.getyear();
 			}
-		int getDay (){
+	boolean isSameMonth (Date otherDate){
+			return this.month==otherDate.getmonth();
+			}
+	boolean isSameDay (Date otherDate){
+			return this.day==otherDate.getday();
+			}
+	int getyear (){
+			return this.year;
+			}
+	int getmonth (){
+			return this.month;
+			}
+	int getday (){
 			return this.day;
 			}
-		void set Day (int day){
+	void setDay (int day){
 			this.day=day;
 			}
-		boolean is Same (Date another){
-			return (isSameYear(another)&&isSameMonth(another)&&isSameDay(another));
+	boolean isSame (Date another){
+			return ((isSameYear(another))&&(isSameMonth(another))&&(isSameDay(another)));
 			}
 	
-		String getMonthName(){
-		String month=newString("");
-	switch (month){
+	String getMonthName(){
+		String month=new String("");
+	switch (this.month){
 		
 		case 1:
-			   if(day>=1&&day<=31){
 			   	
 				month="Enero";
 				break;
-				}
+				
 		case 2:
-			   if(day>=1&&day<=28){
+			    
 				month="Febrero";
 				break;
-				}
+				
 		case 3:
-			   if(day>=1&&day<=31){
+			   
 				month="Marzo";
 				break;
-				}
+				
 		case 4:
-			   if(day>=1&&day<=30){
+			   
 				month="Abril";
 				break;
-				}
+				
 		case 5:
-			   if(day>=1&&day<=31){
+			   
 				month="Mayo";
 				break;
-				}
+				
 		case 6:
-			   if(day>=1&&day<=30){
+			   
 				month="Junio";
 				break;
-				}
+				
 		case 7:
-			   if(day>=1&&day<=31){
+			   
 				month="Julio";
 				break;
-				}
+				
 		case 8:
-			   if(day>=1&&day<=31){
+			 
 				month="Agosto";
 				break;
-				}
+				
 		case 9:
-			   if(day>=1&&day<=30){
+			   
 				month="Septiembre";
 				break;
-				}
+				
 		case 10:
-			   if(day>=1&&day<=31){
+			   
 				month="Octubre";
 				break;
-				}
+				
 		case 11:
-			   if(day>=1&&day<=30){
+			   
 				month="Noviembre";
 				break;
-				}
+				
 		case 12:
-			   if(day>=1&&day<=31){
+			  
 				month="Diciembre";
 				break;
-				}
+				
 		}
-	}
-	public int
+		return (month);
+	} 
 }
